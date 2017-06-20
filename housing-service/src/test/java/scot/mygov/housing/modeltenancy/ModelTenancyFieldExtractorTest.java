22be9b712d7fb718e550a3be66f18bb943a37741
David Sinclair <david.sinclair@gov.scot>
2017-06-20 15:46:03 +0100

    MGS-3196 test coverage

--- housing-service/src/test/java/scot/mygov/housing/modeltenancy/ModelTenancyFieldExtractorTest.java
+++ housing-service/src/test/java/scot/mygov/housing/modeltenancy/ModelTenancyFieldExtractorTest.java
@@ -4,10 +4,14 @@ package scot.mygov.housing.modeltenancy;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
 import org.junit.Test;
+import scot.mygov.housing.modeltenancy.model.CommunicationsAgreement;
 import scot.mygov.housing.modeltenancy.model.ModelTenancy;
 import scot.mygov.housing.modeltenancy.validation.ObjectMother;
 
+import java.time.LocalDate;
 import java.util.Map;
+
+import static java.util.stream.Collectors.joining;
 
 /**
  * Created by z418868 on 20/06/2017.
@@ -27,8 +31,101 @@ public class ModelTenancyFieldExtractorTest {
         Map<String, Object> actual = sut.extractFields(modelTenancy);
 
         // ASSERT
-        Assert.assertTrue(!StringUtils.isEmpty(actual.get("tenantNameAndAddresses").toString()));
+        Assert.assertEquals("(1) name, \taddress", actual.get("tenantNameAndAddresses"));
         Assert.assertTrue(!StringUtils.isEmpty(actual.get("tenantEmails").toString()));
-        Assert.assertTrue(!StringUtils.isEmpty(actual.get("tenantNameAndAddresses").toString()));
+    }
+
+    @Test
+    public void canExtractWithNoLettingAgent() {
+
+        // ARRANGE
+        ModelTenancy modelTenancy = om.anyTenancy();
+        modelTenancy.setLettingAgent(null);
+
+        // ACT
+        Map<String, Object> actual = sut.extractFields(modelTenancy);
+
+        // ASSERT - no fields start with "lettingAgent"
+        Assert.assertEquals("_", actual.get("communicationsAgreementEmail"));
+
+        Assert.assertEquals(0, actual.keySet().stream().filter(key -> key.startsWith("lettingAgent")).count());
+    }
+
+    @Test
+    public void canExtractGuarentorSignatureblock() {
+
+        // ARRANGE
+        ModelTenancy modelTenancy = om.tenancyWithGuarentors();
+
+        // ACT
+        Map<String, Object> actual = sut.extractFields(modelTenancy);
+
+        // ASSERT
+        Assert.assertEquals("(1) name, \taddress", actual.get("tenantNameAndAddresses"));
+    }
+
+
+    @Test
+    public void canExtractHMOFields() {
+
+        // ARRANGE
+        ModelTenancy modelTenancy = om.anyTenancy();
+        modelTenancy.setHmoProperty(true);
+        modelTenancy.setHmoRegistrationExpiryDate(LocalDate.of(2010, 10, 01));
+        modelTenancy.setHmo24ContactNumber("11111");
+
+        // ACT
+        Map<String, Object> actual = sut.extractFields(modelTenancy);
+
+        // ASSERT
+        Assert.assertEquals("is", actual.get("hmoString"));
+        Assert.assertEquals("11111", actual.get("hmoContactNumber"));
+        Assert.assertEquals("01 October 2010", actual.get("hmoExpiryDate"));
+    }
+
+    @Test
+    public void canExtractRentPressureZone() {
+
+        // ARRANGE
+        ModelTenancy modelTenancy = om.anyTenancy();
+        modelTenancy.setInRentPressureZone(true);
+
+        // ACT
+        Map<String, Object> actual = sut.extractFields(modelTenancy);
+
+        // ASSERT
+        Assert.assertEquals("is", actual.get("rentPressureZoneString"));
+    }
+
+    @Test
+    public void canExtractEmailComms() {
+
+        // ARRANGE
+        ModelTenancy modelTenancy = om.anyTenancy();
+        modelTenancy.setCommunicationsAgreement(CommunicationsAgreement.EMAIL.name());
+
+        // ACT
+        Map<String, Object> actual = sut.extractFields(modelTenancy);
+
+        // ASSERT
+        Assert.assertEquals("X", actual.get("communicationsAgreementEmail"));
+        Assert.assertEquals("_", actual.get("communicationsAgreementHardcopy"));
+        Assert.assertEquals("email", actual.get("communicationsAgreementType"));
+    }
+
+    @Test
+    public void canExtractHardCopyComms() {
+
+        // ARRANGE
+        ModelTenancy modelTenancy = om.anyTenancy();
+        modelTenancy.setCommunicationsAgreement(CommunicationsAgreement.HARDCOPY.name());
+
+        // ACT
+        Map<String, Object> actual = sut.extractFields(modelTenancy);
+
+        // ASSERT
+        Assert.assertEquals("_", actual.get("communicationsAgreementEmail"));
+        Assert.assertEquals("X", actual.get("communicationsAgreementHardcopy"));
+        Assert.assertEquals("", actual.get("communicationsAgreementType"));
     }
 }
