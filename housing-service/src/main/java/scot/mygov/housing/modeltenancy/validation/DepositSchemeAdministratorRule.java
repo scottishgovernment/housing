76dd999dfba51d807527f40dbc9f27c43f88e901
David Sinclair <david.sinclair@gov.scot>
2017-08-02 15:51:07 +0100

    MGS-3198 make the deposit scheme administrator optional

--- housing-service/src/main/java/scot/mygov/housing/modeltenancy/validation/DepositSchemeAdministratorRule.java
+++ housing-service/src/main/java/scot/mygov/housing/modeltenancy/validation/DepositSchemeAdministratorRule.java
@@ -1,5 +1,6 @@
 package scot.mygov.housing.modeltenancy.validation;
 
+import org.apache.commons.lang3.StringUtils;
 import scot.mygov.housing.modeltenancy.DepositSchemeAdministrators;
 import scot.mygov.housing.modeltenancy.model.ModelTenancy;
 import scot.mygov.validation.ValidationResultsBuilder;
@@ -11,6 +12,12 @@ public class DepositSchemeAdministratorRule implements ValidationRule<ModelTenan
 
     public void validate(ModelTenancy modelTenancy, ValidationResultsBuilder resultsBuilder) {
 
+        // the field is not mandatory
+        if (StringUtils.isEmpty(modelTenancy.getTenancyDepositSchemeAdministrator())) {
+            return;
+        }
+
+        // if they have specifeid one it must be one we recognise
         if (administrators.forName(modelTenancy.getTenancyDepositSchemeAdministrator()) == null) {
             resultsBuilder.issue("tenancyDepositSchemeAdministrator", "Unrecognised scheme adminsitrator");
         }
