aab697bbaac2ed20c4151c16f44947fcc480d4a3
David Sinclair <david.sinclair@gov.scot>
2017-08-17 14:58:58 +0100

    Extra coverage

---
+++ housing-service/src/test/java/scot/mygov/housing/postcode/MapcloudPostcodeServiceTest.java
@@ -0,0 +1,162 @@
+package scot.mygov.housing.postcode;
+
+import com.codahale.metrics.MetricRegistry;
+import org.junit.Test;
+import org.mockito.Mockito;
+import scot.mygov.housing.MetricName;
+import scot.mygov.housing.mapcloud.Mapcloud;
+import scot.mygov.housing.mapcloud.MapcloudException;
+import scot.mygov.housing.mapcloud.MapcloudResult;
+import scot.mygov.housing.mapcloud.MapcloudResults;
+import scot.mygov.housing.postcode.PostcodeService;
+import scot.mygov.housing.postcode.PostcodeServiceException;
+import scot.mygov.housing.postcode.PostcodeServiceResult;
+import scot.mygov.housing.postcode.PostcodeServiceResults;
+
+import javax.ws.rs.ProcessingException;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+
+public class MapcloudPostcodeServiceTest {
+
+    @Test(expected = PostcodeServiceException.class)
+    public void mapcloudExceptionHandledCorrectly() throws PostcodeServiceException, MapcloudException {
+
+        // ARRANGE
+        PostcodeService sut = new MapcloudPostcodeService(excpetionThrowingMapcloud());
+
+        // ACT
+        sut.lookup(scottishPostcode());
+
+        // ASSERT -- see expected exception
+    }
+
+    @Test
+    public void greenpathResultsParsedCorrectly() throws PostcodeServiceException, MapcloudException {
+
+        // ARRANGE
+        MapcloudResults results = greenpathMapcloudResults();
+        PostcodeService sut = new MapcloudPostcodeService(mapcloudWithResults(results));
+
+        // ACT
+        PostcodeServiceResults actual = sut.lookup(scottishPostcode());
+
+        // ASSERT
+        assertFalse(actual.getResults().isEmpty());
+
+        PostcodeServiceResult result1 = actual.getResults().get(0);
+        assertEquals(result1.getPostcode(), results.getResults().get(0).getPostcode(), scottishPostcode());
+
+        List<String> expectedAddressLines = new ArrayList<>();
+        Collections.addAll(expectedAddressLines,
+                results.getResults().get(0).getAddressLine1(),
+                results.getResults().get(0).getAddressLine2(),
+                results.getResults().get(0).getAddressLine3());
+        assertEquals(expectedAddressLines, result1.getAddressLines());
+        assertEquals(results.getResults().get(0).getUprn(), result1.getUprn());
+        assertEquals(results.getResults().get(0).getTown(), result1.getTown());
+    }
+
+    @Test
+    public void emptyAddressLinesAreOmitted() throws PostcodeServiceException, MapcloudException {
+
+        // ARRANGE
+        MapcloudResults results = greenpathMapcloudResults();
+        // address line 2 should be missing from the results
+        results.getResults().get(0).setAddressLine2("");
+        PostcodeService sut = new MapcloudPostcodeService(mapcloudWithResults(results));
+
+        // ACT
+        PostcodeServiceResults actual = sut.lookup(scottishPostcode());
+
+        // ASSERT
+        assertFalse(actual.getResults().isEmpty());
+
+        PostcodeServiceResult result1 = actual.getResults().get(0);
+        assertEquals(result1.getPostcode(), results.getResults().get(0).getPostcode(), scottishPostcode());
+
+        List<String> expectedAddressLines = new ArrayList<>();
+        Collections.addAll(expectedAddressLines,
+                results.getResults().get(0).getAddressLine1(),
+                results.getResults().get(0).getAddressLine3());
+        assertEquals(expectedAddressLines, result1.getAddressLines());
+        assertEquals(results.getResults().get(0).getUprn(), result1.getUprn());
+        assertEquals(results.getResults().get(0).getTown(), result1.getTown());
+    }
+
+
+    private String scottishPostcode() {
+        return "EH104AX";
+    }
+
+    private Mapcloud excpetionThrowingMapcloud() throws MapcloudException {
+        Mapcloud mapcloud = mock(Mapcloud.class);
+        when(mapcloud.lookupPostcode(any())).thenThrow(new MapcloudException("arg", new RuntimeException("arg")));
+        return mapcloud;
+    }
+
+    private Mapcloud mapcloudWithResults(MapcloudResults results) throws MapcloudException {
+        Mapcloud mapcloud = mock(Mapcloud.class);
+        when(mapcloud.lookupPostcode(any())).thenReturn(results);
+        return mapcloud;
+    }
+
+
+    private WebTarget greenpathTarget(MapcloudResults results) {
+        WebTarget target = mock(WebTarget.class);
+        Invocation.Builder builder = mock(Invocation.Builder.class);
+        when(target.path(any())).thenReturn(target);
+        when(target.queryParam(any(), any())).thenReturn(target);
+        when(target.request()).thenReturn(builder);
+        when(builder.header(any(), any())).thenReturn(builder);
+        when(builder.get(eq(MapcloudResults.class))).thenReturn(results);
+        return target;
+    }
+
+    private WebTarget serverErrorTarget(RuntimeException ex) {
+        WebTarget target = mock(WebTarget.class);
+        Invocation.Builder builder = mock(Invocation.Builder.class);
+        when(target.path(any())).thenReturn(target);
+        when(target.queryParam(any(), any())).thenReturn(target);
+        when(target.request()).thenReturn(builder);
+        when(builder.header(any(), any())).thenReturn(builder);
+        when(builder.get(eq(MapcloudResults.class))).thenThrow(ex);
+        return target;
+    }
+
+    private MapcloudResults greenpathMapcloudResults() {
+        MapcloudResults res = new MapcloudResults();
+        List<MapcloudResult> resultsList = new ArrayList<>();
+        resultsList.add(anyResult());
+        res.setResults(resultsList);
+        return res;
+    }
+
+    private MapcloudResult anyResult() {
+        MapcloudResult res = new MapcloudResult();
+        res.setPostcode(scottishPostcode());
+        res.setUprn("1111");
+        res.setAddressLine1("line1");
+        res.setAddressLine2("line2");
+        res.setAddressLine3("line3");
+        res.setTown("town");
+        return res;
+    }
+
+    private MetricRegistry anyRegistry() {
+        return new MetricRegistry();
+    }
+
+}
