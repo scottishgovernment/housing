a47985a709ea904a68035d7c421418e2520957b4
David Sinclair <david.sinclair@gov.scot>
2017-10-26 10:29:24 +0100

    MGS-3718 added metric collection to mta generation (updated tests)

--- housing-service/src/test/java/scot/mygov/housing/forms/modeltenancy/ModelTenancyServiceTest.java
+++ housing-service/src/test/java/scot/mygov/housing/forms/modeltenancy/ModelTenancyServiceTest.java
@@ -3,6 +3,7 @@ package scot.mygov.housing.forms.modeltenancy;
 import com.aspose.words.Document;
 import com.codahale.metrics.MetricRegistry;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
 import scot.mygov.documents.DocumentGenerator;
@@ -16,6 +17,7 @@ import scot.mygov.housing.forms.modeltenancy.validation.ModelTenancyObjectMother
 
 import java.io.ByteArrayInputStream;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -74,6 +76,48 @@ public class ModelTenancyServiceTest {
         byte [] result = sut.save(modelTenancy, DocumentType.WORD);
 
         // ASSERT - see expected
+    }
+
+
+    @Test
+    public void changedUtilitiesAreCounted() throws Exception {
+
+        // ARRANGE
+        DocumentTemplateLoader templateLoader = templateLoader();
+        DocumentGenerator documentGenerator = new DocumentGenerator(templateLoader);
+        MetricRegistry metricRegistry = new MetricRegistry();
+        ModelTenancyService sut = new ModelTenancyService(documentGenerator, fieldExtractor, metricRegistry);
+        ModelTenancy tenancy = om.anyTenancy();
+        tenancy.getOptionalTerms().setUtilities("changed");
+
+        // ACT
+        sut.save(tenancy, DocumentType.WORD);
+
+        // ASSERT
+        String changedMetricName = MetricRegistry.name(ModelTenancyService.class, "changed-utilities");
+        String unchangedMetricName = MetricRegistry.name(ModelTenancyService.class, "unchanged-utilities");
+        assertEquals(1, metricRegistry.getCounters().get(changedMetricName).getCount());
+        assertEquals(0, metricRegistry.getCounters().get(unchangedMetricName).getCount());
+    }
+
+    @Test
+    public void unchangedUtilitiesAreCounted() throws Exception {
+
+        // ARRANGE
+        DocumentTemplateLoader templateLoader = templateLoader();
+        DocumentGenerator documentGenerator = new DocumentGenerator(templateLoader);
+        MetricRegistry metricRegistry = new MetricRegistry();
+        ModelTenancyService sut = new ModelTenancyService(documentGenerator, fieldExtractor, metricRegistry);
+        ModelTenancy tenancy = om.anyTenancy();
+
+        // ACT
+        sut.save(tenancy, DocumentType.WORD);
+
+        // ASSERT
+        String changedMetricName = MetricRegistry.name(ModelTenancyService.class, "changed-utilities");
+        String unchangedMetricName = MetricRegistry.name(ModelTenancyService.class, "unchanged-utilities");
+        assertEquals(0, metricRegistry.getCounters().get(changedMetricName).getCount());
+        assertEquals(1, metricRegistry.getCounters().get(unchangedMetricName).getCount());
     }
 
     private DocumentTemplateLoader templateLoader() {
