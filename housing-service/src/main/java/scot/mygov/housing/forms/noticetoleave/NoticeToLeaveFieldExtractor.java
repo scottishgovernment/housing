d849d6704e0e6711549fc7cbb817cf76f2e629c5
David Sinclair <david.sinclair@gov.scot>
2018-01-26 13:00:18 +0000

    MGS-3033 I / We behaviour for notice to leave form

--- housing-service/src/main/java/scot/mygov/housing/forms/noticetoleave/NoticeToLeaveFieldExtractor.java
+++ housing-service/src/main/java/scot/mygov/housing/forms/noticetoleave/NoticeToLeaveFieldExtractor.java
@@ -1,7 +1,7 @@
 package scot.mygov.housing.forms.noticetoleave;
 
 import scot.mygov.housing.forms.FieldExtractor;
-import scot.mygov.housing.forms.FieldExtractorUtils;
+import scot.mygov.housing.forms.modeltenancy.model.AgentOrLandLord;
 import scot.mygov.housing.forms.noticetoleave.model.NoticeToLeave;
 import scot.mygov.housing.forms.noticetoleave.model.Reason;
 
@@ -9,18 +9,29 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static java.util.stream.Collectors.joining;
+import static scot.mygov.housing.forms.FieldExtractorUtils.addressFieldsMultipleLines;
+import static scot.mygov.housing.forms.FieldExtractorUtils.formatDate;
+import static scot.mygov.housing.forms.FieldExtractorUtils.naForEmpty;
 
 public class NoticeToLeaveFieldExtractor implements FieldExtractor<NoticeToLeave> {
+
+    private static String I_OR_WE_FIELD = "iOrWe";
+    private static String I_OR_WE_INLINE_FIELD = "iOrWeInline";
+    private static String ARE_OR_AM_FIELD = "areOrAm";
+    private static String LANDLORDS_OR_AGENT_DESCRIPTION_FIELD = "landlordsOrAgentDescrption";
+    private static String LANDLORDS_OR_AGENT_NAMES_AND_ADDRESSES_FIELD = "landlordsOrAgentNamesAndAddresses";
 
     public Map<String, Object> extractFields(NoticeToLeave model) {
 
         Map<String, Object> fields = new HashMap<>();
+        describeLandlordsOrAgent(model, fields);
         fields.put("tenantNames", model.getTenantNames().stream().collect(joining(", ")));
-        fields.put("address", FieldExtractorUtils.addressFieldsMultipleLines(model.getAddress()));
+        fields.put("address", addressFieldsMultipleLines(model.getAddress()));
+        fields.put("entryDate", formatDate(model.getEntryDate()));
         extractReasons(fields, model);
-        fields.put("reasonDetails", model.getReasonDetails());
-        fields.put("supportingEvidence", model.getSupportingEvidence());
-        fields.put("earliestTribunualDate", model.getEarliestTribunualDate());
+        fields.put("reasonDetails", naForEmpty(model.getReasonDetails()));
+        fields.put("supportingEvidence", naForEmpty(model.getSupportingEvidence()));
+        fields.put("earliestTribunualDate", formatDate(model.getEarliestTribunualDate()));
         return fields;
     }
 
@@ -32,4 +43,38 @@ public class NoticeToLeaveFieldExtractor implements FieldExtractor<NoticeToLeave
             fields.put(reason, "X");
         }
     }
+
+    private void describeLandlordsOrAgent(NoticeToLeave model, Map<String, Object> fields) {
+        if (model.getLandlordsAgent() != null) {
+            fields.put(I_OR_WE_FIELD, "I");
+            fields.put(I_OR_WE_INLINE_FIELD, "I");
+            fields.put(ARE_OR_AM_FIELD, "am");
+            fields.put(LANDLORDS_OR_AGENT_DESCRIPTION_FIELD, "Landlord's Agent");
+            fields.put(LANDLORDS_OR_AGENT_NAMES_AND_ADDRESSES_FIELD, formatNamesAndAddress(model.getLandlordsAgent()));
+            return;
+        }
+
+        fields.put("landlordsOrAgentNamesAndAddresses",
+                model.getLandLords().stream()
+                        .map(this::formatNamesAndAddress)
+                        .collect(joining("\n\n")));
+
+        if (model.getLandLords().size() > 1) {
+            fields.put(I_OR_WE_FIELD, "We");
+            fields.put(I_OR_WE_INLINE_FIELD, "we");
+            fields.put(ARE_OR_AM_FIELD, "are");
+            fields.put(LANDLORDS_OR_AGENT_DESCRIPTION_FIELD, "Landlords");
+            return;
+        }
+
+        fields.put(I_OR_WE_FIELD, "I");
+        fields.put(ARE_OR_AM_FIELD, "am");
+        fields.put(I_OR_WE_INLINE_FIELD, "I");
+        fields.put(LANDLORDS_OR_AGENT_DESCRIPTION_FIELD, "Landlord");
+    }
+
+    private String formatNamesAndAddress(AgentOrLandLord person) {
+        return String.format("%s Of\n%s", person.getName(), addressFieldsMultipleLines(person.getAddress()));
+    }
+
 }
