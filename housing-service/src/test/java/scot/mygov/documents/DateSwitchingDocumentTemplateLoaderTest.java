186552637bc4bec827acae68d2f0941ba4de5c84
Martin Ellis <martin.ellis@gov.scot>
2021-03-16 16:54:57 +0000

    Improvements to DateSwitchingDocumentTemplateLoader and tests
    Fix date in test for the day after a new document should be used.
    The old test was duplicating the assertion in a previous test.
    
    Use floorEntry instead of traversing the map of templates explictly.
    
    Fix typos and formatting.

--- housing-service/src/test/java/scot/mygov/documents/DateSwitchingDocumentTemplateLoaderTest.java
+++ housing-service/src/test/java/scot/mygov/documents/DateSwitchingDocumentTemplateLoaderTest.java
@@ -4,67 +4,65 @@ import com.aspose.words.Document;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-
 import java.time.LocalDate;
 
 import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
-/**
- * Created by z418868 on 25/09/2020.
- */
 public class DateSwitchingDocumentTemplateLoaderTest {
 
     @Test
     public void usesOldTemplateForDateBeforeCovidDate() {
         DateSwitchingDocumentTemplateLoader sut = new DateSwitchingDocumentTemplateLoader();
-        DocumentTemplateLoader oldLoader = Mockito.mock(DocumentTemplateLoader.class);
-        DocumentTemplateLoader newLoader = Mockito.mock(DocumentTemplateLoader.class);
+        DocumentTemplateLoader oldLoader = mock(DocumentTemplateLoader.class);
+        DocumentTemplateLoader newLoader = mock(DocumentTemplateLoader.class);
         sut.addDocument(LocalDate.of(2009, 01, 01), oldLoader);
         sut.addDocument(LocalDate.of(2010, 01, 01), newLoader);
-        sut.localDateSuplier = () -> LocalDate.of(2009, 10, 01);
+        sut.localDateSupplier = () -> LocalDate.of(2009, 10, 01);
 
         // ACT
         sut.loadDocumentTemplate();
 
         // ASSERT
-        Mockito.verify(newLoader, never()).loadDocumentTemplate();
-        Mockito.verify(oldLoader, times(1)).loadDocumentTemplate();
+        verify(newLoader, never()).loadDocumentTemplate();
+        verify(oldLoader, times(1)).loadDocumentTemplate();
     }
 
     @Test
     public void usesNewTemplateForDateOnCovidDate() {
         DateSwitchingDocumentTemplateLoader sut = new DateSwitchingDocumentTemplateLoader();
-        DocumentTemplateLoader oldLoader = Mockito.mock(DocumentTemplateLoader.class);
-        DocumentTemplateLoader newLoader = Mockito.mock(DocumentTemplateLoader.class);
+        DocumentTemplateLoader oldLoader = mock(DocumentTemplateLoader.class);
+        DocumentTemplateLoader newLoader = mock(DocumentTemplateLoader.class);
         sut.addDocument(LocalDate.of(2009, 01, 01), oldLoader);
         sut.addDocument(LocalDate.of(2010, 01, 01), newLoader);
-        sut.localDateSuplier = () -> LocalDate.of(2010, 01, 01);
+        sut.localDateSupplier = () -> LocalDate.of(2010, 01, 01);
 
         // ACT
         sut.loadDocumentTemplate();
 
         // ASSERT
-        Mockito.verify(oldLoader, never()).loadDocumentTemplate();
-        Mockito.verify(newLoader, times(1)).loadDocumentTemplate();
+        verify(oldLoader, never()).loadDocumentTemplate();
+        verify(newLoader, times(1)).loadDocumentTemplate();
     }
 
     @Test
-    public void usesRighttmeplateForDateAfterCovidDate() {
+    public void usesRightTemplateForDateAfterCovidDate() {
         DateSwitchingDocumentTemplateLoader sut = new DateSwitchingDocumentTemplateLoader();
-        DocumentTemplateLoader oldLoader = Mockito.mock(DocumentTemplateLoader.class);
-        DocumentTemplateLoader newLoader = Mockito.mock(DocumentTemplateLoader.class);
+        DocumentTemplateLoader oldLoader = mock(DocumentTemplateLoader.class);
+        DocumentTemplateLoader newLoader = mock(DocumentTemplateLoader.class);
         sut.addDocument(LocalDate.of(2009, 01, 01), oldLoader);
         sut.addDocument(LocalDate.of(2010, 01, 01), newLoader);
-        sut.localDateSuplier = () -> LocalDate.of(2020, 01, 01);
+        sut.localDateSupplier = () -> LocalDate.of(2020, 01, 02);
 
         // ACT
         sut.loadDocumentTemplate();
 
         // ASSERT
-        Mockito.verify(oldLoader, never()).loadDocumentTemplate();
-        Mockito.verify(newLoader, times(1)).loadDocumentTemplate();
+        verify(oldLoader, never()).loadDocumentTemplate();
+        verify(newLoader, times(1)).loadDocumentTemplate();
     }
 
     @Test
