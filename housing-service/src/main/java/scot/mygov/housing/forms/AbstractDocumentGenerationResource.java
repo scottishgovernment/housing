21182cfa6aa78162d0da254c42bdfbe61adc3edc
David Sinclair <david.sinclair@gov.scot>
2023-04-24 14:37:20 +0100

    MSG-7201 provide implementation of GET in form generation resources
    This stops the housing service from alerting when we see GET requests.

--- housing-service/src/main/java/scot/mygov/housing/forms/AbstractDocumentGenerationResource.java
+++ housing-service/src/main/java/scot/mygov/housing/forms/AbstractDocumentGenerationResource.java
@@ -4,11 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
 import scot.mygov.documents.DocumentType;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.Encoded;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
@@ -44,6 +40,20 @@ public abstract class AbstractDocumentGenerationResource<T extends AbstractFormM
         return response(model, params.get("type"));
     }
 
+    /**
+     * We think that some users press refresh after downloading the form causinf a get request.  Without this method
+     * this is caught by the expcetion handler and the service alerts.
+     */
+    @Path("form")
+    @GET
+    public Response get() {
+        String html = "<htlm><body>" +
+                "<p>GET not supported, please return to start of form.</p>" +
+                "</body></html>";
+        return Response.status(Response.Status.METHOD_NOT_ALLOWED)
+                .type(MediaType.TEXT_HTML_TYPE)
+                .entity(html).build();
+    }
 
     private Response response(T model, String typeParam)
             throws DocumentGenerationServiceException {
